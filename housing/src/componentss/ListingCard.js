9b99d9cd84f930e9944318231f167dbef50ec488
bhagtanib <bbhagtani23@my.whitworth.edu>
2023-04-09 13:24:51 -0700

    added house list

--- housing/src/componentss/ListingCard.js
+++ housing/src/componentss/ListingCard.js
@@ -4,6 +4,12 @@ const ListingCard = () => {
   return (
     <div className={classes.card}>
         <div className={classes.left}></div>
+        <div className={classes.rightBg} ></div>
+        <div className={classes.right}>
+          <p className={classes.price}> $3200</p>
+          <p className={classes.desc}>A nice house with blah bakah A nice house with blah bakah</p>
+          <p className={classes.btn}>&gt;</p>
+        </div>
     </div>
   )
 }
